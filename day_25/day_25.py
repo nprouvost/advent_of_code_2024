9861d0512ec415affc7b156373b496eaf3d3850a
Nathan Prouvost <nathan.prouvost@gmail.com>
2024-12-25 20:53:17 +0100

    add day 25

---
+++ day_25/day_25.py
@@ -0,0 +1,82 @@
+# whatever imports you need
+from itertools import combinations
+import numpy as np
+
+
+# create example code:
+example = [
+    "2333133121414131402",
+]
+
+example2 = [
+    "12345",
+]
+
+
+# read file to list of lines
+def read_file_to_list(file_path):
+    with open(file_path, 'r') as file:
+        lines = [line.strip() for line in file]
+    return lines
+
+
+def main():
+    file_path = 'day_25.txt'
+    lines_array = read_file_to_list(file_path)
+    # lines_array = example
+
+    i = 0
+    keys = []
+    locks = []
+    is_key = False
+    is_lock = False
+    for line in lines_array:
+        if line == "":
+            continue
+        else:
+            i = i % 7
+            if i == 0:
+                is_lock = False
+                is_key = False
+                if line == "#####":
+                    is_lock = True
+                    lock = []
+                else:
+                    is_key = True
+                    key = []
+
+            if is_key:
+                key.append(list(line))
+                if i == 6:
+                    keys.append(key)
+            elif is_lock:
+                lock.append(list(line))
+                if i == 6:
+                    locks.append(lock)
+            else:
+                raise ValueError("line is not part of a key or a lock")
+            i += 1
+
+    keys = np.array(keys)
+    locks = np.array(locks)
+    locks_len = []
+    for lock in locks:
+        locks_len.append([sum(lock[:, i] == "#") for i in range(len(lock[0]))])
+
+    keys_len = []
+    for key in keys:
+        keys_len.append([sum(key[:, i] == "#") for i in range(len(key[0]))])
+
+    keys_len = np.array(keys_len)
+    locks_len = np.array(locks_len)
+
+    unique_pairs = set()
+    for ikey, key_len in enumerate(keys_len):
+        for ilock, lock_len in enumerate(locks_len):
+            if np.all((key_len + lock_len) < 8):
+                unique_pairs.add((ikey, ilock))
+    print("Part 1:", len(unique_pairs))
+
+
+if __name__ == "__main__":
+    main()
